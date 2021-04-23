cacf91dad4790f0fcc18ef2a6f1eaae10fc16320
Brayan Muñoz <brayanmnz@hotmail.com>
2021-04-23 11:32:18 -0400

    Servicio SOAP creado

---
+++ app/src/main/java/Parcial2_Web/util/FormularioWebServices.java
@@ -0,0 +1,35 @@
+package Parcial2_Web.util;
+
+
+import Parcial2_Web.Classes.*;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import java.util.*;
+
+/**
+ * Clase para implementar un servicio web basado en SOAP
+ */
+@WebService
+public class FormularioWebServices {
+
+    private FormularioServicios serviciosFormularios = FormularioServicios.getInstance();
+
+    @WebMethod
+    public String holaMundo(String hola){
+        System.out.println("Ejecuntado en el servidor.");
+        return "Hola Mundo "+hola+", :-D";
+    }
+
+    @WebMethod
+    public String otroMetodo(String hola){
+        System.out.println("Ejecuntado en el servidor.");
+        return "Hola Mundo "+hola+", :-D";
+    }
+
+    // @WebMethod
+    // public List<Formulario> getListaFormularios(){
+    //     return serviciosFormularios.findAll();
+    // }
+
+
+}
